775331ef37974d2cb2dc7c54a94e63d16fe698ef
Dennis Lincango <76795089+dennis-lincango@users.noreply.github.com>
2024-05-26 00:34:50 -0500

    refactor: reduce lines of code

--- src/minigames/memory/card.gd
+++ src/minigames/memory/card.gd
@@ -1,14 +1,11 @@
 extends Area2D
 
-var card_name
-var card_face
-var card_back
+var card_name = "Empty"
+var card_face = preload("res://assets/minigames/memory/box.png")
+var card_back = preload("res://assets/minigames/memory/box.png")
 var click_enabled = true
 
 func _ready():
-    card_name = "Empty"
-    card_face = preload("res://assets/minigames/memory/box.png")
-    card_back = preload("res://assets/minigames/memory/box.png")
     get_node("Sprite").texture = card_back
 
 func _input_event(viewport, event, shape_idx):
@@ -17,16 +14,23 @@ func _input_event(viewport, event, shape_idx):
 
 func on_click():
     if click_enabled:
-        if get_parent().last_try_was_pair:
-            get_parent().last_try_was_pair = false
-            get_parent()._reset_card_name_strings_and_check_box()
         click_enabled = false
         get_node("Sprite").texture = card_face
-        if get_parent().get_node("CardOneName").text == "Card 1":
-            get_parent().get_node("CardOneName").text = card_name
-            get_parent().card_one_checked_if_pairing = name
-        elif get_parent().get_node("CardOneName").text != "Card 1":
-            if get_parent().get_node("CardTwoName").text == "Card 2":
-                get_parent().get_node("CardTwoName").text = card_name
-                get_parent().card_two_checked_if_pairing = name
-                get_parent()._check_if_pair()
+        _handle_card_click()
+
+func _handle_card_click():
+    var parent = get_parent()
+    var card_one_name_node = parent.get_node("CardOneName")
+    var card_two_name_node = parent.get_node("CardTwoName")
+
+    if parent.last_try_was_pair:
+        parent.last_try_was_pair = false
+        parent._reset_card_name_strings_and_check_box()
+
+    if card_one_name_node.text == "Card 1":
+        card_one_name_node.text = card_name
+        parent.card_one_checked_if_pairing = name
+    elif card_two_name_node.text == "Card 2":
+        card_two_name_node.text = card_name
+        parent.card_two_checked_if_pairing = name
+        parent._check_if_pair()
