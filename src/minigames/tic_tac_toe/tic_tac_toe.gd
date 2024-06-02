8ce9ae835380f82e26926b470a814d7385cf34d6
Dennis Lincango <76795089+dennis-lincango@users.noreply.github.com>
2024-06-02 11:01:42 -0500

    feat: transition of scenary

--- src/minigames/tic_tac_toe/tic_tac_toe.gd
+++ src/minigames/tic_tac_toe/tic_tac_toe.gd
@@ -9,71 +9,90 @@ var turn : int = 0
 
 var is_game_end : bool = false
 
+var transition_player : AnimationPlayer = null
+
 func _ready():
-	for cell_count in range(9):
-		var cell = Cell.instantiate()
-		cell.main = self
-		$Cells.add_child(cell)
-		cells.append(cell)
-		cell.cell_updated.connect(_on_cell_updated)
+    for cell_count in range(9):
+        var cell = Cell.instantiate()
+        cell.main = self
+        $Cells.add_child(cell)
+        cells.append(cell)
+        cell.cell_updated.connect(_on_cell_updated)
+    
+    transition_player = get_node_or_null("TransitionPlayer")
+    if transition_player:
+        transition_player.connect("animation_finished", _on_animation_player_animation_finished)
+    else:
+        print("Error: TransitionPlayer no encontrado.")
 
 func _on_cell_updated(cell):
-	randomize()
-	var match_result = check_match()
-	
-	print(match_result)
-	
-	if match_result:
-		is_game_end = true
-		start_win_animation(match_result)
-	
-	elif play_with == "AI" and turn == 1:
-		var ai_cell = cells[randi() % cells.size()]
-		if ai_cell.cell_value == "":
-			ai_cell.draw_cell()
-		else:
-			_on_cell_updated(cell)
+    randomize()
+    var match_result = check_match()
+    
+    print(match_result)
+    
+    if match_result:
+        is_game_end = true
+        start_win_animation(match_result)
+    
+    elif play_with == "AI" and turn == 1:
+        var ai_cell = cells[randi() % cells.size()]
+        if ai_cell.cell_value == "":
+            ai_cell.draw_cell()
+        else:
+            _on_cell_updated(cell)
 
 func _on_restart_button_pressed():
-	get_tree().reload_current_scene()
+    get_tree().reload_current_scene()
 
 func check_match():
-	for h in range(3):
-		if cells[0+3*h].cell_value == "X" and cells[1+3*h].cell_value == "X" and cells[2+3*h].cell_value == "X":
-			return ["X", 1+3*h, 2+3*h, 3+3*h]
-	for v in range(3):
-		if cells[0+v].cell_value == "X" and cells[3+v].cell_value == "X" and cells[6+v].cell_value == "X":
-			return ["X", 1+v, 4+v, 7+v]
-	if cells[0].cell_value == "X" and cells[4].cell_value == "X" and cells[8].cell_value == "X":
-		return ["X", 1, 5, 9]
-	elif cells[2].cell_value == "X" and cells[4].cell_value == "X" and cells[6].cell_value == "X":
-		return ["X", 3, 5, 7]
-	
-	for h in range(3):
-		if cells[0+3*h].cell_value == "O" and cells[1+3*h].cell_value == "O" and cells[2+3*h].cell_value == "O":
-			return ["O", 1+3*h, 2+3*h, 3+3*h]
-	for v in range(3):
-		if cells[0+v].cell_value == "O" and cells[3+v].cell_value == "O" and cells[6+v].cell_value == "O":
-			return ["O", 1+v, 4+v, 7+v]
-	if cells[0].cell_value == "O" and cells[4].cell_value == "O" and cells[8].cell_value == "O":
-		return ["O", 1, 5, 9]
-	elif cells[2].cell_value == "O" and cells[4].cell_value == "O" and cells[6].cell_value == "O":
-		return ["O", 3, 5, 7]
-	
-	var full = true
-	for cell in cells:
-		if cell.cell_value == "":
-			full = false
-	
-	if full: return["Draw", 0, 0, 0]
+    for h in range(3):
+        if cells[0+3*h].cell_value == "X" and cells[1+3*h].cell_value == "X" and cells[2+3*h].cell_value == "X":
+            return ["X", 1+3*h, 2+3*h, 3+3*h]
+    for v in range(3):
+        if cells[0+v].cell_value == "X" and cells[3+v].cell_value == "X" and cells[6+v].cell_value == "X":
+            return ["X", 1+v, 4+v, 7+v]
+    if cells[0].cell_value == "X" and cells[4].cell_value == "X" and cells[8].cell_value == "X":
+        return ["X", 1, 5, 9]
+    elif cells[2].cell_value == "X" and cells[4].cell_value == "X" and cells[6].cell_value == "X":
+        return ["X", 3, 5, 7]
+    
+    for h in range(3):
+        if cells[0+3*h].cell_value == "O" and cells[1+3*h].cell_value == "O" and cells[2+3*h].cell_value == "O":
+            return ["O", 1+3*h, 2+3*h, 3+3*h]
+    for v in range(3):
+        if cells[0+v].cell_value == "O" and cells[3+v].cell_value == "O" and cells[6+v].cell_value == "O":
+            return ["O", 1+v, 4+v, 7+v]
+    if cells[0].cell_value == "O" and cells[4].cell_value == "O" and cells[8].cell_value == "O":
+        return ["O", 1, 5, 9]
+    elif cells[2].cell_value == "O" and cells[4].cell_value == "O" and cells[6].cell_value == "O":
+        return ["O", 3, 5, 7]
+    
+    var full = true
+    for cell in cells:
+        if cell.cell_value == "":
+            full = false
+    
+    if full: return ["Draw", 0, 0, 0]
 
 func start_win_animation(match_result: Array):
-	var color: Color
-	
-	if match_result[0] == "X":
-		color = Color.BLUE
-	elif match_result[0] == "O":
-		color = Color.RED
-	
-	for c in range(3):
-		cells[match_result[c+1]-1].glow(color)
+    var color: Color
+    
+    if match_result[0] == "X":
+        color = Color.BLUE
+    elif match_result[0] == "O":
+        color = Color.RED
+    else :
+        color = Color.WHITE
+    
+    for c in range(3):
+        cells[match_result[c+1]-1].glow(color)
+    
+    if transition_player:
+        transition_player.play("slide_transition")
+    else:
+        print("Error: TransitionPlayer no encontrado.")
+
+func _on_animation_player_animation_finished(anim_name):
+    if anim_name == "slide_transition":
+        get_tree().reload_current_scene()
